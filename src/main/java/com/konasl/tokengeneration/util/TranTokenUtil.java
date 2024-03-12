5240368e444f59370e692a534d81619c3152ce09
atuar <atuar.cse@gmail.com>
2024-03-12 13:49:48 +0600

    code clean

--- src/main/java/com/konasl/tokengeneration/util/TranTokenUtil.java
+++ src/main/java/com/konasl/tokengeneration/util/TranTokenUtil.java
@@ -6,7 +6,7 @@ import org.apache.commons.codec.digest.HmacUtils;
 
 import java.nio.charset.Charset;
 
-public class TranTokenUtil {git 
+public class TranTokenUtil {
 
     public static String generateToken(String currentTime, String secretKey, String message) {
         return "KMV1:" + currentTime + ":" + generateHMac(secretKey, message);
